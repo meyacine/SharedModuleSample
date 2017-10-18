795719b293a8c2b0bf9a2b5fb526a3484e76bd15
ymeddah <my.meddah@gmail.com>
2017-10-18 22:04:16 +0200

    lazy loaded module preparing ngx-translate integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
 import { LeftMenuComponent } from './left-menu/left-menu.component';
+import {TranslateModule} from '@ngx-translate/core';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,14 @@ import { LeftMenuComponent } from './left-menu/left-menu.component';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    RouterModule.forRoot([
+      {
+        path: 'busines',
+        loadChildren: './busines-sample/busines-sample.module#BusinesSampleModule'
+      }
+    ]),
+    SharedModule,
+    TranslateModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule
   ]
 })
 export class SharedModule { }
